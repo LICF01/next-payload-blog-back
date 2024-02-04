2f3593553cc874216a459e01cd708f0c7a6e8e03
LICF01 <lucascubilla@gmail.com>
2024-02-04 09:57:50 -0300

    Add Pages and Media collections with some blocks

---
+++ src/blocks/Hero.ts
@@ -0,0 +1,21 @@
+import { Block } from "payload/types";
+
+export const Hero: Block = {
+  slug: "hero",
+  labels: {
+    singular: "Hero Block",
+    plural: "Hero Blocks",
+  },
+  fields: [
+    {
+      name: "heading",
+      label: "Heading",
+      type: "text",
+    },
+    {
+      name: "text",
+      label: "Text",
+      type: "textarea",
+    },
+  ],
+};
